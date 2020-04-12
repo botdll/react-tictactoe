96a8616f28c15e72b752e41a179d201401b82cd9
George Tsilikas <tsili.g@gmail.com>
2020-04-12 04:40:46 +0300

    updated

--- src/Game.js
+++ src/Game.js
@@ -19,6 +19,8 @@ class Game extends Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = [...current.squares];
+        // const squares = current.squares.slice();
+
 
         if (calculateWinner(squares) || squares[i]) {
             return;
